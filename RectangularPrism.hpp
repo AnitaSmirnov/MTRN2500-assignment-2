96e5f075e85988fa7898a70f7df665e5c743d10b
AnitaSmirnov <38997541+AnitaSmirnov@users.noreply.github.com>
2018-09-04 11:24:08 +1000

    Add files via upload
    rectangular prism header file created #5m

---
+++ RectangularPrism.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "Shape.hpp"
+#include <Windows.h>
+#include <tchar.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+
+class RectangularPrism : public Shape
+{
+public:
+	RegtangularPrism();
+	RectangularPrism(double x_, double y_, double z_, double x_length, double y_length, double z_length, double rotation_);
+	~RectangularPrism();
+	void draw();
+protected:
+	double x_l, y_l, z_l;		  // lengths
+};
