1d405ab514f56037ed9112211ba356350e35d8fa
AnitaSmirnov <38997541+AnitaSmirnov@users.noreply.github.com>
2018-09-04 13:38:14 +1000

    Add files via upload
    rectangular prism source file done + tested #25m

---
+++ RectangularPrism.cpp
@@ -0,0 +1,74 @@
+// Rectangular prism
+#include "RectangularPrism.hpp"
+
+RectangularPrism::RectangularPrism() {
+	x = y = z = 0.0;
+	x_l = y_l = z_l = 1.0;
+	rotation = 0.0;
+	red = green = blue = 1.0;
+};
+
+RectangularPrism::~RectangularPrism() {
+}
+
+RectangularPrism::RectangularPrism(double x_, double y_, double z_, double x_length, double y_length, double z_length, double rotation_) {
+	x = x_;
+	y = y_;
+	z = z_;
+	
+	x_l = x_length;
+	y_l = y_length;
+	z_l = z_length;
+
+	rotation = rotation_;
+	red = green = blue = 1.0;
+}
+
+void RectangularPrism::draw() {
+	glTranslated(10.0, 0, 10.0);
+	glRotated(rotation, 0.0, 1.0, 0.0);
+	glBegin(GL_QUADS);
+	// top
+	glNormal3d(0.0, 1.0, 0.0);
+	glVertex3d(x + x_l / 2, y + y_l, z + z_l / 2);
+	glVertex3d(x - x_l / 2, y + y_l, z + z_l / 2);
+	glVertex3d(x - x_l / 2, y + y_l, z - z_l / 2);
+	glVertex3d(x + x_l / 2, y + y_l, z - z_l / 2);
+
+	// front
+	glNormal3d(0.0, 0.0, 1.0);
+	glVertex3d(x - x_l / 2, y, z + z_l / 2);
+	glVertex3d(x - x_l / 2, y + y_l, z + z_l / 2);
+	glVertex3d(x + x_l / 2, y + y_l, z + z_l / 2);
+	glVertex3d(x + x_l / 2, y, z + z_l / 2);
+
+	// right
+	glNormal3d(1.0, 0.0, 0.0);
+	glVertex3d(x + x_l / 2, y, z - z_l / 2);
+	glVertex3d(x + x_l / 2, y, z + z_l / 2);
+	glVertex3d(x + x_l / 2, y + y_l, z + z_l / 2);
+	glVertex3d(x + x_l / 2, y + y_l, z - z_l / 2);
+
+	// left
+	glNormal3d(-1.0, 0.0, 0.0);
+	glVertex3d(x - x_l / 2, y, z - z_l / 2);
+	glVertex3d(x - x_l / 2, y, z + z_l / 2);
+	glVertex3d(x - x_l / 2, y + y_l, z + z_l / 2);
+	glVertex3d(x - x_l / 2, y + y_l, z - z_l / 2);
+
+	// bottom
+	glNormal3d(0.0, -1.0, 0.0);
+	glVertex3d(x + x_l/2, y, z + z_l/2);
+	glVertex3d(x - x_l / 2, y, z + z_l / 2);
+	glVertex3d(x - x_l / 2, y, z - z_l / 2);
+	glVertex3d(x + x_l / 2, y, z - z_l / 2);
+
+	// back
+	glNormal3d(0.0, 0.0, -1.0);
+	glVertex3d(x - x_l / 2, y, z - z_l / 2);
+	glVertex3d(x - x_l / 2, y + y_l, z - z_l / 2);
+	glVertex3d(x + x_l / 2, y + y_l, z - z_l / 2);
+	glVertex3d(x + x_l / 2, y, z - z_l / 2);
+
+	glEnd();
+}
--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <map>
 
+
 #ifdef __APPLE__
 	#include <OpenGL/gl.h>
 	#include <OpenGL/glu.h>
@@ -38,6 +39,8 @@
 #include "Messages.hpp"
 #include "HUD.hpp"
 #include "ObstacleManager.hpp"
+
+#include "RectangularPrism.hpp"
 
 void display();
 void reshape(int width, int height);
@@ -155,16 +158,83 @@ void drawGoals()
 
 // define new draw functions here
 void drawSquares() {
-	glColor3f(1, 0, 0.6);
-	glRectf(20, 20, -20, -20);
-	glColor3f(0.6, 0, 1);
-	glRectf(30, 30, -10, -10);
+	//glColor3f(1, 0, 0.6);
+	//glRectf(20, 20, -20, -20);
+	//glRotatef(45, 0, 1, 0);
+	//glColor3f(0.6, 0, 1);
+	//glRectf(30, 30, -10, -10);
+	glBegin(GL_QUADS);
+	// top
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glNormal3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(-0.5f, 0.5f, 0.5f);
+	glVertex3f(0.5f, 0.5f, 0.5f);
+	glVertex3f(0.5f, 0.5f, -0.5f);
+	glVertex3f(-0.5f, 0.5f, -0.5f);
 
+	glEnd();
+
+	glBegin(GL_QUADS);
+	// front
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glNormal3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.5f, -0.5f, 0.5f);
+	glVertex3f(0.5f, 0.5f, 0.5f);
+	glVertex3f(-0.5f, 0.5f, 0.5f);
+	glVertex3f(-0.5f, -0.5f, 0.5f);
+
+	glEnd();
+
+	glBegin(GL_QUADS);
+	// right
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glNormal3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(0.5f, 0.5f, -0.5f);
+	glVertex3f(0.5f, 0.5f, 0.5f);
+	glVertex3f(0.5f, -0.5f, 0.5f);
+	glVertex3f(0.5f, -0.5f, -0.5f);
+
+	glEnd();
+
+	glBegin(GL_QUADS);
+	// left
+	glColor3f(0.0f, 0.0f, 0.5f);
+	glNormal3f(-1.0f, 0.0f, 0.0f);
+	glVertex3f(-0.5f, -0.5f, 0.5f);
+	glVertex3f(-0.5f, 0.5f, 0.5f);
+	glVertex3f(-0.5f, 0.5f, -0.5f);
+	glVertex3f(-0.5f, -0.5f, -0.5f);
+
+	glEnd();
+
+	glBegin(GL_QUADS);
+	// bottom
+	glColor3f(0.5f, 0.0f, 0.0f);
+	glNormal3f(0.0f, -1.0f, 0.0f);
+	glVertex3f(0.5f, -0.5f, 0.5f);
+	glVertex3f(-0.5f, -0.5f, 0.5f);
+	glVertex3f(-0.5f, -0.5f, -0.5f);
+	glVertex3f(0.5f, -0.5f, -0.5f);
+
+	glEnd();
+
+	glBegin(GL_QUADS);
+	// back
+	glColor3f(0.0f, 0.5f, 0.0f);
+	glNormal3f(0.0f, 0.0f, -1.0f);
+	glVertex3f(0.5f, 0.5f, -0.5f);
+	glVertex3f(0.5f, -0.5f, -0.5f);
+	glVertex3f(-0.5f, -0.5f, -0.5f);
+	glVertex3f(-0.5f, 0.5f, -0.5f);
+
+	glEnd();
 }
 
-void drawTrig() {
-	glColor3f(0, 1, 0.2);
-	
+void drawTest() {
+	RectangularPrism test(0.0, 0.0, 0.0, 12.0, 5.0, 7.0, 45.0);
+	test.setColor(1, 0.0, 0.1);
+	test.setColorInGL();
+	test.draw();
 }
 
 
@@ -212,7 +282,8 @@ void display() {
 
 	// call any new draw functions here!
 	//drawSquares();
-
+	drawTest();
+	
 	glutSwapBuffers();
 };
 
