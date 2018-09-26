2f98ab0b99e1f3e809251d5b2fd6b93c07fcddff
Alexgendary <voquoctrung.bi@gmail.com>
2018-09-26 09:54:58 +0200

    Add files via upload
    added bool roll and bool steer; needs to make them actually rolling and steering

--- Cylinder.cpp
+++ Cylinder.cpp
@@ -1,9 +1,9 @@
-//
 //  Cylinder.cpp
 //  Assignment 2
 //
 //  Created by Alex Trung Vo on 4/9/18.
-//  Copyright © 2018 Anita & Alex. All rights reserved.
+//  Edited by Anita Smirnov
+//  Copyright � 2018 Anita & Alex. All rights reserved.
 //
 
 #ifdef __APPLE__
@@ -18,39 +18,69 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
-#else
-#include <GL/gl.h>
-#include <GL/glu.h>
-#include <GL/glut.h>
-#include <unistd.h>
-#include <sys/time.h>
 #endif
 
 #include "Cylinder.hpp"
 
-Cylinder::Cylinder() : Shape(0,0,0){
-    radius = 5.0;
-    depth = 1.0;
-};
+Cylinder::Cylinder() {
+	x = z = rotation = 0.0;
+	y = 0.5;
+	radius = 5.0;
+	depth = 1.0;
+	red = blue = green = 1.0;
+    roll = steer = false;
+}
 
-Cylinder::Cylinder(double x_, double y_, double z_, double radius_, double depth_) : Shape(x_,y_,z_){
-    radius = radius_;
-    depth = depth_;
-};
+Cylinder::Cylinder(double x_, double y_, double z_, double radius_, double depth_, double rotation_,float red_, float green_, float blue_, bool roll_, bool steer_) {
+	x = x_;
+	y = y_;
+	z = z_;
+	rotation = rotation_;
+	radius = radius_;
+	depth = depth_;
+	red = red_;
+	blue = blue_;
+	green = green_;
+    roll = roll_;
+    steer = steer_;
+}
 
-Cylinder::~Cylinder(){
-};
+Cylinder::~Cylinder() {
+}
 
-void Cylinder::draw(){
-    glPushMatrix();
-    positionInGL();
-    glColor3d(red, green, blue);
+void Cylinder::set_roll(bool roll_){
+    roll = roll_;
+}
+void Cylinder::set_steer(bool steer_){
+    steer = steer_;
+}
+
+bool Cylinder::get_roll(){
+    return roll;
+}
+bool Cylinder::get_steer(){
+    return steer;
+}
+void Cylinder::draw() {
+	glColor3d(red, green, blue);
+	int n = 5;
+	glPushMatrix();
+	glTranslated(x, y, z);
+	glRotated(rotation, 0.0, 1.0, 0.0);
+	glBegin(GL_QUADS);
+	GLUquadric * q = gluNewQuadric();
+	gluCylinder(q, radius, radius, depth, n, n);
+	gluDisk(q, 0, radius, n, n);
+	glTranslated(0, 0, depth);
+	gluDisk(q, 0, radius, n, n);
     
-    GLUquadricObj *q = gluNewQuadric();
+    if(roll){
+        glVertex3d(-radius, -radius, depth);
+        glVertex3d(-radius, radius, depth);
+        glVertex3d(radius, radius, depth);
+        glVertex3d(radius, -radius, depth);
+    }
     
-    glTranslated(0, radius, -depth/2);
-    int n = 30;
-    
-    gluCylinder(q, radius, radius, depth, n, n);
-    gluDisk(q, 0, radius, n, n);
-};
+	glEnd();
+	glPopMatrix();
+}
--- Cylinder.hpp
+++ Cylinder.hpp
@@ -1,29 +1,44 @@
 //  Assignment 2
 //
 //  Created by Alex Trung Vo on 4/9/18.
-//  Copyright © 2018 Anita & Alex. All rights reserved.
+//  Copyright 2018 Anita & Alex. All rights reserved.
 //
 #pragma once
 #ifndef Cylinder_hpp
 #define Cylinder_hpp
 
 #include <stdio.h>
+#include "Shape.hpp"
+
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#elif defined(WIN32)
 #include <Windows.h>
-#include <tchar.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
-#include "Shape.hpp"
+#endif
 
 class Cylinder : public Shape {
 protected:
 	double radius;
 	double depth;
+    bool roll;
+    bool steer;
 
 public:
 	Cylinder();
-	Cylinder(double x, double y, double z, double radius_, double depth_, double rotation_);
+	Cylinder(double x, double y, double z, double radius_, double depth_, double rotation_,float red_, float green_, float blue_, bool roll_, bool steer_);
 	~Cylinder();
+    
+    void set_roll(bool roll_);
+    void set_steer(bool steer_);
+    
+    bool get_roll();
+    bool get_steer();
+    
 	void draw();
 };
 #endif
