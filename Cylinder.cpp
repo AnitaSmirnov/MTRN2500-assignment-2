b489de56ff618f8d36975af70994db5c58daa13b
AnitaSmirnov <38997541+AnitaSmirnov@users.noreply.github.com>
2018-10-02 19:30:32 +1000

    Add files via upload
    getRadius and getDepth functions added

---
+++ Cylinder.cpp
@@ -0,0 +1,82 @@
+//  Cylinder.cpp
+//  Assignment 2
+//
+//  Created by Alex Trung Vo on 4/9/18.
+//  Edited by Anita Smirnov
+//  Copyright © 2018 Anita & Alex. All rights reserved.
+//
+
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#include <unistd.h>
+#include <sys/time.h>
+#elif defined(WIN32)
+#include <Windows.h>
+#include <tchar.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#endif
+
+#include "Cylinder.hpp"
+
+#include "KeyManager.hpp"
+
+Cylinder::Cylinder() {
+	x = z = rotation = 0.0;
+	y = 0.5;
+	radius = 5.0;
+	depth = 1.0;
+	red = blue = green = 1.0;
+}
+
+Cylinder::Cylinder(double x_, double y_, double z_, double radius_, double depth_, double rotation_, float red_, float green_, float blue_, bool roll_, bool steer_) {
+	x = x_;
+	y = y_;
+	z = z_;
+	rotation = rotation_;
+	radius = radius_;
+	depth = depth_;
+	red = red_;
+	green = green_;
+	blue = blue_;
+	roll = roll_;
+	steer = steer_;
+}
+
+Cylinder::~Cylinder() {
+}
+
+void Cylinder::draw() {
+	glColor3f(red, green, blue);
+	int n = 5;
+	glPushMatrix();
+	glTranslated(x, y, z);
+	glRotated(rotation, 0.0, 1.0, 0.0);
+	glBegin(GL_POLYGON);
+	GLUquadric * q = gluNewQuadric();
+	gluCylinder(q, radius, radius, depth, n, n);
+	gluDisk(q, 0, radius, n, n);
+	glTranslated(0, 0, depth);
+	gluDisk(q, 0, radius, n, n);
+	glEnd();
+	glPopMatrix();
+}
+
+bool Cylinder::getSteer() {
+	return steer;
+}
+
+bool Cylinder::getRoll() {
+	return roll;
+}
+
+float Cylinder::getRadius() {
+	return (float)radius;
+}
+
+float Cylinder::getDepth() {
+	return (float)depth;
+}
---
+++ Cylinder.hpp
@@ -0,0 +1,34 @@
+//  Assignment 2
+//
+//  Created by Alex Trung Vo on 4/9/18.
+//  Copyright © 2018 Anita & Alex. All rights reserved.
+//
+#pragma once
+#ifndef Cylinder_hpp
+#define Cylinder_hpp
+
+#include <stdio.h>
+#include <Windows.h>
+#include <tchar.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#include "Shape.hpp"
+
+class Cylinder : public Shape {
+protected:
+	double radius;
+	double depth;
+	bool roll;
+	bool steer;
+public:
+	Cylinder();
+	Cylinder(double x, double y, double z, double radius_, double depth_, double rotation_, float red_, float green_, float blue_, bool roll_, bool steer_);
+	~Cylinder();
+	void draw();
+	bool getSteer();
+	bool getRoll();
+	float getRadius();
+	float getDepth();
+};
+#endif
